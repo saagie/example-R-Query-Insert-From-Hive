db849ff4b6b50b43535816684f201da280a974d4
Tony <tony@saagie.com>
2019-12-16 11:34:12 +0100

    feat: Added ODBC and Kerberos option

---
+++ ODBC/hive_querying_ODBC.R
@@ -0,0 +1,84 @@
+library(DBI) # Standard database interface
+library(odbc) # Connect to ODBC databases using DBI
+library(dplyr) # Data manipulation library
+library(dbplyr) # Converts data manipulation written in R to SQL
+
+########## Connection without Kerberos ##########
+
+con <- dbConnect(odbc::odbc(),
+                 Driver   = "Cloudera ODBC Driver for Apache Hive 64-bit",
+                 Host     = "nn1.pX.company.prod.saagie.io",
+                 Port     = 10000,
+                 Schema   = "default",
+                 AuthMech = 3,
+                 UseSASL  = 1,
+                 UID      = "user",
+                 PWD      = "password")
+
+
+########## Connection with Kerberos ##########
+
+library(getPass)
+# Method 1 (interactive) : Use in Rstudio. Interactive pop up to enter password
+system('kinit user',input=getPass('Enter your password: '))
+
+# Method 2 (scripts) : Use outside of Rstudio. 
+# Password is written in command line or stored in a environment variable
+# Uncomment next line to use
+# system('echo password | kinit user')
+
+con <- dbConnect(odbc::odbc(),
+                 Driver   = "Cloudera ODBC Driver for Apache Hive 64-bit",
+                 Host     = "nn1.pX.company.prod.saagie.io",
+                 Port     = 10000,
+                 Schema   = "default",
+                 AuthMech = 1)
+
+
+########## Execute queries on connection ##########
+
+# List all tables from all schemas
+dbGetQuery(con, 'show schemas')
+dbGetQuery(con, 'show tables')
+
+sample <- DBI::dbReadTable(con, "sample") # Fetch all data from a table
+
+# Execute sql statement on a connection
+dbGetQuery(con, 'select count(*) from sample')
+forecast <- dbGetQuery(con, 'select * from forecast.forecast_weekly limit 200')
+
+# Create a lazy tbl from an Impala table
+forecast_db <- tbl(con, in_schema('forecast', 'forecast_weekly'))
+forecast_db # Print columns and column types
+
+# The query is written in dplyr syntax, but executed on a remote sql database
+query <- forecast_db %>% 
+  summarise(mean_forecast = forecast %>% as.numeric() %>% mean)
+
+show_query(query) # Show the query that will be executed
+query # Executes the lazy query when the result is printed
+
+# Example of usable dplyr verbs
+forecast_db %>% 
+  filter(prediction_date == max(prediction_date)) %>%
+  group_by(reference) %>%
+  summarise(forecast_mean = mean(forecast), 
+            forecast_max = max(forecast), 
+            nb_forecast_total = n())
+
+sales_db <- tbl(con, in_schema('forecast', 'sales_weekly'))
+sales_db
+
+forecast_db <- forecast_db %>%
+  mutate(reference = as.character(reference))
+
+diff_db <- inner_join(forecast_db, sales_db, by=c('reference', 'year', 'week'))
+
+diff_by_ref <- diff_db %>% 
+  group_by(reference, year, week) %>% 
+  summarise(diff_by_week = abs(forecast - quantity)) %>% # Difference between forecast and reality for each prediction
+  group_by(reference) %>%
+  summarise(diff_by_ref = sum(diff_by_week)) # Sum of all differences for each reference
+
+diff_db # Executes all the lazy queries above 
+
--- README.md
+++ README.md
@@ -1,27 +1,17 @@
-# Query and Insert From Hive
+Query and Insert From Hive
+=================
 
-## Dependencies
-- R package `RJDBC` -> allows R to connect to any DBMS that has a JDBC driver
-- Impala JDBC Drivers
+2 options are available, ODBC or JDBC.
 
-## Usage
+## ODBC (Recommended)
 
-- Fill in the variable `hiveConnectionUrl`, the url used to open a connection on Hive
-- Replace the value `Hive_Jdbc_Folder_Path` with the name of the hive drivers folder in the function `JDBC()`
-- If your system is a Windows, add this code in the beginning in your script :
+- Uses proprietary drivers to connect to Impala
+- Direct connection
+- Better integration thanks to odbc and others packages
 
-  - Load/Download DBI library :
-  `if(!require("DBI")){ 
-  install.packages("DBI", repos = "http://cran.r-project.org")
-  library("DBI")
-  }`
+## JDBC
 
-  - JAVA_HOME = "" :
-  `if(Sys.getenv("JAVA_HOME")!="")
-    Sys.setenv(JAVA_HOME="")`
-
-  - Load/Download rJava library :
-  `if(!require("rJava")){
-    install.packages("rJava", repos = "http://cran.r-project.org")
-    library("rJava")
-  }`
+- Uses Java to connect to Impala
+- Requires an installed JVM
+- The JVM must have enough RAM to retrive data from the database (if you want to fetch a 2GB table from Impala, you'll need 2 GB in the JVM + 2GB in your R session)
+- Very basic integration
